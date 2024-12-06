cacc2466ac8763788539c6d346f0d951b6f88477
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2024-12-06 16:31:11 +0530

    New questions Practiced

--- 8.array/duplicate.cpp
+++ 8.array/duplicate.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 int main()
 {
-    int arr[]={1, 2, 1, 3, 4, 1};
+    int arr[]={1, 2, 1, 3, 4, 1, 2, 3};
     
-    for(int i=0; i<6; ++i)
+    for(int i=0; i<8; ++i)
     {
-        for(int j=i+1; j<6; ++j)
+        // int key=arr[i];
+       int count=0;
+        for(int j=i+1; j<8; ++j)
         {
-            if(arr[i]==arr[j])
+            if(arr[i]==arr[j] && arr[i]!=-1)
             {
-                cout<<"duplicate value is-"<<arr[i];
-                arr[j]=-j;
+                count++;
+                arr[j]=-1;
             }
-            
+        }
+        if(count>0)
+        {
+            cout<<"Duplicate value is:"<<arr[i]<<" "<<count<<" times repeated\n";
         }
     }
 }
