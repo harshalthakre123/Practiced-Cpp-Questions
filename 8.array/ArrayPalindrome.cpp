4b40a8ab67827f0652ac9b8846ddc9c50c30cad2
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2024-12-10 23:41:45 +0530

    Array palindrome and other questions

--- 8.array/ArrayPalindrome.cpp
+++ 8.array/ArrayPalindrome.cpp
@@ -2,18 +2,90 @@
 using namespace std;
 int main()
 {
+    int n=5;
+    int arr[n]={1, 2, 1, 2, 1}; 
 
-    int arr[n]={1, 2, 1, 2, 1}
-    int n=5, y=arr[n];
+    // //Method 1;---------------------------------------------------------------------------------------
+
+    // int array[n];
+    // // array[]=arr[];
+
     
-    for(int i=0; i<n/2; ++i)
+    // for(int i=0; i<n; ++i)
+    // {
+    //     array[i]=arr[i];
+    // }
+
+
+    // for(int i=0; i<n/2; ++i)
+    // {
+    //     int temp= arr[n-i-1];
+    //     arr[n-i-1]=arr[i];
+    //     arr[i]=temp;
+    // }
+
+
+    // for(int i=0; i<n; ++i)
+    // {
+    //     if(array[i]!=arr[i])
+    //     {
+    //         cout<<"Array is not Palindrome.";
+    //         return 0;
+    //     }
+    // }
+    // cout<<"Array is Palindrome.";
+
+
+
+    // //Method 2;---------------------------------------------------------------------------------------------
+
+    // bool palindrome=true;
+    // for(int i=0; i<n/2; ++i)
+    // {
+    //     int temp= arr[n-i-1];
+    //     if(arr[i]!=temp)
+    //     {
+    //         palindrome=false;
+    //     }
+    //     arr[n-i-1]=arr[i];
+    //     arr[i]=temp;
+        
+    // }
+    // if(palindrome==false)
+    // {
+    //     cout<<"It is not Palindrome.";
+    // }
+    // else
+    // {
+    //     cout<<"It is Palindrome.";
+    // }
+
+    // //Method-3;
+
+    // int s=0;
+    // int e=4;
+    // while(s<=e)
+    // {
+    //     if(arr[s]!=arr[e])
+    //     {
+    //         cout<<"Not a Palindrome.";
+    //         return 0;
+    //     }
+    //     s++;
+    //     e--;
+    // }
+    // cout<<"palindrome";
+
+    // //Method-4;
+
+    for(int i=0; i<=n/2; ++i)
     {
-        int temp= arr[n-i-1];
-        arr[n-i-1]=arr[i];
-        arr[i]=temp;
+        if(arr[i]!=arr[n-i-1])
+        {
+            cout<<"Not a Palindrome.";
+            return 0;
+        }
     }
-    if(y==arr[n])
-    {
-        cout<<"Array is Palindrome";
-    }
+    cout<<"Palindrome";
+
 }
--- 8.array/Target.cpp
+++ 8.array/Target.cpp
@@ -4,6 +4,17 @@ int main()
 {
     int arr[]={1, 2, 4, 3, 4, 1};
 
+    // for(int i=1; i<6; ++i)
+    // {
+    //     if(arr[i]==5)
+    //     {
+    //         cout<<"found\n";
+    //         return 0;
+    //     }
+            
+    // }
+    // cout<<"not found";
+
     for(int i=1; i<6; ++i)
     {
         if(arr[i]==5)
